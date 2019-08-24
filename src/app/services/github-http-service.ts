529dde461de67b56f2549cf26ffa5abb7b341d83
Wachira Beatrice <BeatriceWambui>
2019-08-24 22:34:54 +0300

    create on the http search

--- src/app/services/github-http-service.ts
+++ src/app/services/github-http-service.ts
@@ -1,6 +1,12 @@
 import { Injectable } from '@angular/core'
 import { HttpClient } from 'selenium-webdriver/http';
-import { environment } from '../'
+import { environment } from '../../environments/environment';
+import { Github } from '../github';
+
+    @Injectable({
+        providedIn: 'root'
+    })
+
 
 export class GithubHttpService {
     github:Github[]=[];
@@ -8,6 +14,26 @@ export class GithubHttpService {
 
     searchGithub(searchTerm:string){
 
-        let
+        let searchEndPoint=" "
+        searchEndPoint += "&q="+searchTerm;
+        let promise = new Promise((resolve, reject)=>{
+            this.http.get(searchEndPoint).toPromise().then(
+                (results)=>{
+                    this.github=[];
+                    for( let i=0; i<results["data"].length;i++){
+                        let url = results ["data"] [i]["images"]["fixed_height"]["url"];
+                        let gitt = new Github(url);
+                        this.github.push(gitt);
+                    }
+                    console.log(this.github);
+                    resolve()
+                },
+                (error)=>{
+                    console.log(error)
+                    reject()
+                }
+            )
+        })
+        return promise;
     }
 }
