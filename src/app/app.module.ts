07148ad42ba6e899566c86d6097db3712429c972
Wachira Beatrice <BeatriceWambui>
2019-08-24 18:16:40 +0300

    generate search form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GithubNavbarComponent } from './github-navbar/github-navbar.component';
+import { GithubSearchFormComponent } from './github-search-form/github-search-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GithubNavbarComponent
+    GithubNavbarComponent,
+    GithubSearchFormComponent
   ],
   imports: [
     BrowserModule,
