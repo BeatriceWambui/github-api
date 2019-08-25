a8bee503e6dad3d4bd8ab9bd728752e274fd3da5
Wachira Beatrice <BeatriceWambui>
2019-08-26 00:35:14 +0300

    add github http service ts

--- src/app/services/github-http-service.ts
+++ src/app/services/github-http-service.ts
@@ -1,39 +1,59 @@
 import { Injectable } from '@angular/core'
-import { HttpClient } from 'selenium-webdriver/http';
+import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Github } from '../github';
+import { Repo } from '../repo';
 
-    @Injectable({
-        providedIn: 'root'
-    })
-
+@Injectable({
+    providedIn: 'root'
+})
 
 export class GithubHttpService {
-    github:Github[]=[];
-    constructor(private http:HttpClient){}
+    repoEndPoint = "https://api.github.com/users/";
+    github: Github[] = [];
+    repo: Repo[];
+    constructor(private http: HttpClient) { }
 
-    searchGithub(searchTerm:string){
+    searchGithub(searchTerm: string) {
+        interface myData {
+            login: string;
+            url: string;
+        }
+        let searchEndPoint = "https://api.github.com/users/";
 
-        let searchEndPoint=" "
-        searchEndPoint += "&q="+searchTerm;
-        let promise = new Promise((resolve, reject)=>{
-            this.http.get(searchEndPoint).toPromise().then(
-                (results)=>{
-                    this.github=[];
-                    for( let i=0; i<results["data"].length;i++){
-                        let url = results ["data"] [i]["images"]["fixed_height"]["url"];
-                        let gitt = new Github(url);
-                        this.github.push(gitt);
-                    }
-                    console.log(this.github);
-                    resolve()
+        let promise = new Promise((resolve, reject) => {
+            this.github = [];
+            this.http.get<myData>(searchEndPoint + searchTerm + '?access_token='+environment.GITHUBAPIKEY).toPromise().then(
+                (results) => {
+                    this.github.push(results);
+                    resolve();
                 },
-                (error)=>{
-                    console.log(error)
+                (error) => {
+                    console.log(error);
                     reject()
                 }
             )
         })
         return promise;
+    };
+
+    getRepositories(searchTerm: string) {
+        interface repo{
+            name: string;
+        }
+        // url = 'https://api.github.com/users/beatricewambui/repos?access_token=dac03084a42944088d5ae55a2454e7f026e07f3b';
+        this.repoEndPoint += "/repos?=" + searchTerm + environment.GITHUBAPIKEY;
+        return new Promise((resolve, reject) => {
+            this.http.get<repo>('https://api.github.com/users/'+ searchTerm + '/repos?access_token=' + environment.GITHUBAPIKEY).toPromise().then(
+                (result) => {
+                    this.repo = result; 
+                    resolve();
+                },
+                (error) => {
+                    console.log(error);
+                    reject();
+                })
+        })
+
     }
 }
